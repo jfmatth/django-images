70bf57ae091548cc627c3d05f6eadc410a8e8c47
John Matthew <jmatthew@publicstorage.com>
2025-12-02 13:20:22 -0800

    Streaming features

--- images/tables.py
+++ images/tables.py
@@ -1,12 +1,19 @@
 import django_tables2 as tables
-
+from django_tables2.utils import A
 from images.models import Image
 
-
 class ImageTable(tables.Table):
-    preview = tables.TemplateColumn('<img src="{{record.file.url}}" class="img-fluid" height="25" width="25">')
+    preview = tables.TemplateColumn(
+        '<img src="{{record.file.url}}" class="img-fluid" height="25" width="25">'
+    )
+
+    imageurl = tables.LinkColumn(
+        "image-stream",
+        kwargs={"pk": tables.A("pk")},
+        text="Link"
+    )
 
     class Meta:
         model = Image
         orderable = False
-        fields = ("title","file", "uploaded_at")
+        fields = ("preview", "title", "uploaded_at", "imageurl")
