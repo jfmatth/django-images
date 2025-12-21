258a192a710345d02ad482ac1de9762963fa426f
John Matthew <john@compunique.com>
2025-12-20 20:21:46 -0800

    Remove async warnings from using the dev media server

--- images/tables.py
+++ images/tables.py
@@ -3,9 +3,9 @@ from django_tables2.utils import A
 from images.models import Image
 
 class ImageTable(tables.Table):
-    preview = tables.TemplateColumn(
-        '<img src="{{record.file.url}}" class="img-fluid" height="25" width="25">'
-    )
+    # preview = tables.TemplateColumn(
+    #     '<img src="{{record.file.url}}" class="img-fluid" height="25" width="25">'
+    # )
 
     imageurl = tables.LinkColumn(
         "image-stream",
@@ -13,7 +13,13 @@ class ImageTable(tables.Table):
         text="Link"
     )
 
+    size = tables.Column(empty_values=[])
+
     class Meta:
         model = Image
         orderable = False
-        fields = ("preview", "title", "uploaded_at", "imageurl")
+        # fields = ("preview", "title", "uploaded_at", "imageurl")
+        fields = ( "title", "uploaded_at", "size", "imageurl")
+
+    def render_size(self, record):
+        return record.file.size
