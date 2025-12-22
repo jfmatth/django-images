fb7b37dc36ce17b67249d8d1ad3db61a78ad2546
John Matthew <john@compunique.com>
2025-12-22 10:34:24 -0800

    change to async aiofiles, improved tuning for pods

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 import logging
 from asgiref.sync import sync_to_async
-from asyncio import sleep
+from asyncio import sleep, get_running_loop
+import aiofiles
 
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
@@ -35,39 +36,26 @@ class UploadView(CreateView):
     success_url = reverse_lazy("images-index")
 
 class ImageStreamView(View):
-    # async def get(self, request, pk, *args, **kwargs):
-    #     # Fetch object asynchronously (wrap ORM call)
-    #     try:
-    #         image_obj = await sync_to_async(Image.objects.get)(pk=pk)
-    #     except Image.DoesNotExist:
-    #         raise Http404("Image not found")
-
-    #     # Generator to stream chunks
-    #     async def image_iterator(chunk_size=64*1024):
-    #         data = image_obj.filedata
-    #         for i in range(0, len(data), chunk_size):
-    #             yield data[i:i+chunk_size]
-
-    #     response = StreamingHttpResponse(
-    #         image_iterator(),
-    #         content_type="image/png"
-    #     )
-    #     response["Content-Length"] = len(image_obj.filedata)
-    #     response["Content-Disposition"] = f'inline; filename="{image_obj.file}"'
-
-    #     return response
-
     async def get(self, request, pk):
         image_obj = await sync_to_async(Image.objects.get)(pk=pk)
         file = image_obj.file.open("rb")  # if using FileField
 
-        async def iterator(chunk_size=64*1024):
-            while True:
-                chunk = await sync_to_async(file.read)(chunk_size)
-                if not chunk:
-                    break
-                yield chunk
+        async def iterator(chunk_size=64 * 1024):
+            async with aiofiles.open(file.path, "rb") as f:
+                while True:
+                    chunk = await f.read(chunk_size)
+                    if not chunk:
+                        break
+                    yield chunk
+
+        # async def iterator(chunk_size=64*1024):
+        #     while True:
+        #         chunk = await sync_to_async(file.read)(chunk_size)
+        #         if not chunk:
+        #             break
+        #         yield chunk
 
         response = StreamingHttpResponse(iterator(), content_type="image/png")
         response["Content-Length"] = image_obj.file.size
+
         return response
